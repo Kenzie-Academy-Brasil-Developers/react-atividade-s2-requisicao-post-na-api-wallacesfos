a8563532c67bd3618430dcb487067485542130f1
wallacesfos <wallacesfo@hotmail.com>
2021-09-21 16:04:00 -0300

    feito

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import * as React from 'react';
+import Formulario from './Components/Formulario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Formulario />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Formulario/index.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {TextField, Button} from '@material-ui/core';
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function Formulario(){
+
+    const [name, setName] = useState('')
+    const [senha, setSenha] = useState('')
+    const [requests, setRequests] = useState('')
+    const [Login, setLogin] = useState(false)
+
+
+    const handleForm = event => {
+        event.preventDefault()
+        
+        const obj = {
+            username: name,
+            password: senha,
+        }
+        
+        axios.post('https://kenzieshop.herokuapp.com/sessions/', obj).then((response) => {
+            setRequests('Autenticado com Sucesso!!!')  
+            setLogin(true)          
+        })
+        .catch((err) =>{
+            setRequests('Erro na autenticação')
+            setLogin(false)          
+        })
+    }
+
+    return (
+        <div className={Login? 'verde' : 'vermelho'}>
+            <form onSubmit={handleForm}>
+                <div className="labelText">
+                    <TextField label="Nome" color="primary" variant="outlined" onChange={(e) => setName(e.target.value)} />
+                </div>
+
+                <div className="labelText">
+                    <TextField label="Senha" color="primary" variant="outlined" onChange={(e) => setSenha(e.target.value)} />
+                </div>
+                <Button type="submit" className="botao" variant="contained">Entrar</Button>
+            </form>
+            {requests}
+        </div>
+    )
+}
